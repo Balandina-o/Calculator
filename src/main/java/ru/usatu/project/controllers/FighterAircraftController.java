03338f86e7076745fd08b74c6fb17f6af036848f
balandina-o <balandina.olga.2021@gmail.com>
2023-02-15 20:17:22 +0500

    Создание главной страницы, создание каркаса программы

---
+++ src/main/java/ru/usatu/project/controllers/FighterAircraftController.java
@@ -0,0 +1,44 @@
+package ru.usatu.project.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.usatu.project.models.FighterAircraft;
+import ru.usatu.project.services.FighterAircraftService;
+
+@Controller
+@RequiredArgsConstructor
+public class FighterAircraftController {
+    private final FighterAircraftService fighterAircraftService;
+
+
+    @GetMapping ("/")
+    public String fighterAircraftPage (Model model){
+        model.addAttribute("fighterAircrafts", fighterAircraftService.getCalculatorFighterAircraftsList());
+        return "fighterAircraftPage";
+    }
+
+    @GetMapping("/fighterAircraft/delete/{id}")
+    public String fighterAircraftInfo (@PathVariable Long id, Model model){
+        model.addAttribute("fighterAircraft", fighterAircraftService.getFighterAircraftByID(id));
+
+        return "fighterAircraft-info";
+    }
+
+    @PostMapping("/fighterAircraft/create")
+    public String createFighterAircraft(FighterAircraft fighterAircraft){
+        fighterAircraftService.saveFighterAircraft(fighterAircraft);
+        return "redirect:/";
+
+    }
+    @PostMapping("/fighterAircraft/delete/{id}")
+    public String deleteFighterAircraft(@PathVariable Long id){
+        fighterAircraftService.deleteFighterAircraft(id);
+        return "redirect:/";
+    }
+
+}
---
+++ src/main/java/ru/usatu/project/models/FighterAircraft.java
@@ -0,0 +1,15 @@
+package ru.usatu.project.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FighterAircraft {
+    private long id;
+    private String perem1;
+    private String perem2;
+    private String perem3;
+    private String perem4;
+    private String perem5;
+}
